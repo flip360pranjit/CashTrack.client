3f2acbd0faf00a26c97c40589c173c1e10555090
Piyush Kumar <ayushpiyush419@gmail.com>
2023-04-07 17:50:35 +0530

    Added About page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar, Footer } from "./components/index";
 import { Home, Error, Authentication } from "./pages/index";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
           <Route path="/authenticate" element={<Authentication />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
       <Footer />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./Navbar.module.scss";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return <div className={style.navbar}>Navbar</div>;
 }
 
 export default Navbar;
